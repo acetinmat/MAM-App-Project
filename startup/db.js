70473a166d0c089f91cd366e9fb9eeb785878037
acetinmat <acetinmat@gmail.com>
2022-08-08 13:22:09 +0300

    Modified db.js

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,8 @@ module.exports = function() {
     const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/mam-app';
     mongoose.connect(connectionString)
         .then(() => console.log('Connected to MongoDB...'))
-        .catch((err) => console.log('Could not connected to the MongoDB: ', err));
+        .catch((err) => { 
+            console.log('Could not connected to the MongoDB: ', err);
+            process.exit(1);
+        });
 }
