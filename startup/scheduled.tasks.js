b8ea16ad53d56067317c63a20a6947f7724b6ea4
acetinmat <acetinmat@gmail.com>
2022-08-18 09:58:42 +0300

    Add env variables

--- startup/scheduled.tasks.js
+++ startup/scheduled.tasks.js
@@ -3,14 +3,24 @@ const logger = require('../controlers/logger');
 const Data = require('../models/data.model');
 
 module.exports = function () {
-    cron.schedule('30 0 * * *', async () => {
-        // Delete records older than a week
-        try {
-            await Data.deleteMany({ createdAt: { $lte: Date.now() - 7 * 60 * 60 * 24 * 1000 }});
-            logger.info('*****Old records are deleted from database.*****');            
-        } catch (error) {
-            logger.error(error.message, error);
-        }
-    });
+
+    const days_before = process.env.DAYS_BEFORE || 7;    // Default: 7 days
+    const timezone_string = process.env.TIMEZONE || "Europe/Istanbul";
+
+    cron.schedule(
+        '30 0 * * *',
+        async () => {
+            try {
+                await Data.deleteMany({ createdAt: { $lte: Date.now() - days_before * 24 * 60 * 60 * 1000 } });
+                logger.info('*****Old records are deleted from database.*****');
+            } catch (error) {
+                logger.error(error.message, error);
+            }
+        },
+        {
+            scheduled: true,
+            timezone: timezone_string
+        });
+    
     logger.info('A task that removing old records scheduled: Everyday at 00.30');
 }
