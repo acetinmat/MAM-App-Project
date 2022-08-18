9f0e7141d6236426331084f789a20f41b573199d
acetinmat <acetinmat@gmail.com>
2022-08-18 10:01:38 +0300

    Dockerize app

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('../controlers/logger');
 
 module.exports = function() {
-    const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/mam-app';
+    const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongodb:27017/mam-app';
     mongoose.connect(connectionString)
         .then(() => logger.info('Connected to MongoDB...'));
 }
